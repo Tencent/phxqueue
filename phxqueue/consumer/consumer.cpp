21ee314a2ea8a850d345748829b42e957b9dca0e
taohe <taohex@gmail.com>
2018-02-28 11:15:32 +0800

    fix bug

--- phxqueue/consumer/consumer.cpp
+++ phxqueue/consumer/consumer.cpp
@@ -1075,3 +1075,10 @@ void Consumer::AfterConsume(const comm::proto::ConsumerContext &cc,
 
 }  // namespace phxqueue
 
+//gzrd_Lib_CPP_Version_ID--start
+#ifndef GZRD_SVN_ATTR
+#define GZRD_SVN_ATTR "0"
+#endif
+static char gzrd_Lib_CPP_Version_ID[] __attribute__((used))="$HeadURL$ $Id$ " GZRD_SVN_ATTR "__file__";
+// gzrd_Lib_CPP_Version_ID--end
+
