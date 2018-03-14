b7138977554c98c6201b2bb4833f2a251da2bb0f
unixliang <junjieliang@qq.com>
2018-03-14 12:04:27 +0800

    fix merge

--- phxqueue/config/utils/sub_util.cpp
+++ phxqueue/config/utils/sub_util.cpp
@@ -75,10 +75,3 @@ comm::RetCode GetSubIDsByConsumerAddr(const int topic_id, const comm::proto::Add
 }  // namespace phxqueue
 
 
-//gzrd_Lib_CPP_Version_ID--start
-#ifndef GZRD_SVN_ATTR
-#define GZRD_SVN_ATTR "0"
-#endif
-static char gzrd_Lib_CPP_Version_ID[] __attribute__((used))="$HeadURL$ $Id$ " GZRD_SVN_ATTR "__file__";
-// gzrd_Lib_CPP_Version_ID--end
-
